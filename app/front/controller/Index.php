efeafdf9b6c7b7121fbe11a85d4268564c759096
johnsonzz <liz@uclbrt.com>
2017-05-28 11:40:46 +0800

    angular

--- app/front/controller/Index.php
+++ app/front/controller/Index.php
@@ -6,14 +6,6 @@ class Index
 {
     public function index()
     {
-		die;
-        $arry = [
-           ['id'=>2,'title'=>'t2'],
-           ['id'=>null,'title'=>'t3'],
-           ['id'=>null,'title'=>'t4'],
-           ['id'=>null,'title'=>'t5'],
-           ['id'=>7,'title'=>'t7'],
-        ];
         $tpl = new zane\Template();
 		$tpl->assign('data','hello world');
 		$tpl->assign('person','zane');
